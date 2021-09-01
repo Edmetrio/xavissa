98f2884559066a8086198ede790bc481b085d48f
Edmetrio <edmetrio06@gmail.com>
2021-09-01 13:56:35 +0200

    header de pagamento

---
+++ resources/views/headerPagamento.blade.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="pt-pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First Tech</title>
+
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/bootstrap.min.css')}}">
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/font.awesome.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="{{asset('assets/css/vendor/ionicons.min.css')}}">
+    <!-- Slick CSS -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/slick.min.css')}}">
+    <!-- Animation -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/animate.min.css')}}">
+    <!-- jQuery Ui -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/jquery-ui.min.css')}}">
+    <!-- Nice Select -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/nice-select.min.css')}}">
+    <!-- Magnific Popup -->
+    <link rel="stylesheet" href="{{asset('assets/css/plugins/magnific-popup.css')}}">
+
+    <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
+
+    <!-- Script Sislog para Pagamento via visa -->
+    <script type="text/javascript" src="https://lin4.sislog.com/mobile/c2b_visa/bim_mastercard.js">
+    </script>
+    <script src="https://millenniumbim.gateway.mastercard.com/checkout/version/58/checkout.js" 
+    data-error="erroreCallback" data-cancel="cancelCallback" data-complete="completeCallback">
+    </script>
+
+
+</head>
+
+<body>
+
+    <div class="main-header">
+        <div class="container container-default custom-area">
+            <div class="row">
+                <div class="col-lg-12 col-custom">
+                    <div class="row align-items-center">
+                        <div class="col-lg-2 col-xl-2 col-sm-6 col-6 col-custom">
+                            <div class="header-logo d-flex align-items-center">
+                                <a href="{{url('/')}}">
+                                    <img class="img-full" src="{{asset('assets/images/logo/logo.png')}}" alt="Header Logo">
+                                </a>
+                            </div>
+                        </div>
+                        <div class="col-lg-8 col-xl-7 position-static d-none d-lg-block col-custom">
+                            <nav class="main-nav d-flex justify-content-center">
+                                <ul class="nav">
+                                    <li>
+                                        <a href="{{url('/')}}">
+                                            <span class="menu-text"> Inicio</span>
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="{{url('categoria')}}">
+                                            <span class="menu-text">Categorias</span>
+                                            <i class="fa fa-angle-down"></i>
+                                        </a>
+                                        <ul class="dropdown-submenu dropdown-hover">
+                                            @foreach($categoria as $c)
+                                            <li><a href="{{url('produto/'.$c->id)}}">{{$c->nome}}</a></li>
+                                            @endforeach
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="{{url('produto')}}">
+                                            <span class="menu-text"> Todos Produtos</span>
+                                        </a>
+                                    </li>
+
+                                    <li>
+                                        <a href="{{'historico'}}">
+                                            <span class="menu-text"> Sobre Nos</span>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <span class="menu-text">Contactos</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-lg-2 col-xl-3 col-sm-6 col-6 col-custom">
+                            <div class="header-right-area main-nav">
+                                <ul class="nav">
+                                    <li class="login-register-wrap d-none d-xl-flex">
+                                        <!-- <span><a href="login.html">Entrar</a></span>
+                                        <span><a href="register.html">Registrar</a></span> -->
+                                        @if (Route::has('login'))
+                                        @auth
+                                        <span>
+                                            <div>{{ Auth::user()->name }}</div>
+                                        </span>
+                                        <span>
+                                            <form method="POST" action="{{ route('logout') }}">
+                                                @csrf
+
+                                                <x-dropdown-link :href="route('logout')" onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                                    {{ __('Sair') }}
+                                                </x-dropdown-link>
+                                            </form>
+                                        </span>
+                                        @else
+                                        <span>
+                                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Entrar</a>
+                                        </span>
+                                        @if (Route::has('register'))
+                                        <span>
+                                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Registrar</a>
+                                        </span>
+                                        @endif
+                                        @endauth
+                                        @endif
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @yield('content')
+
+</body>
+
+</html>
